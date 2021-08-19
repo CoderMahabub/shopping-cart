e7647691ab6d0e141d4803c669605c3c8a2be2ae
CoderMahabub <codermahabub@gmail.com>
2021-08-19 13:23:09 +0600

    27-4 Get all product sub total cost, tax and grand total

--- app.js
+++ app.js
@@ -11,6 +11,29 @@ function updateProductNumber(product, price, isIncreasing) {
     //update Casetotal
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    // Calculate Total
+    calculateTotal();
+
+    function getInputValue(product) {
+        const productInput = document.getElementById(product + '-number');
+        const productNumber = parseInt(productInput.value);
+        return productNumber;
+    }
+    function calculateTotal() {
+        /* const phoneInput = document.getElementById('phone-number');
+        const phoneNumber = parseInt(phoneInput.value); */
+
+        const phoneTotal = getInputValue('phone') * 1219;
+        const caseTotal = getInputValue('case') * 59;
+        const subTotal = phoneTotal + caseTotal;
+        const tax = subTotal / 10;
+        const totalPrice = subTotal + tax;
+        // update on the html 
+        document.getElementById('sub-total').innerText = subTotal;
+        document.getElementById('tax-amount').innerText = tax;
+        document.getElementById('total-price').innerText = subTotal + tax;
+
+    }
 }
 // handle phone increase decreas events
 document.getElementById('phone-plus').addEventListener('click', function () {
